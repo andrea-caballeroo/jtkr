186bf1db4bb0f0a45c1bf7462f04c9185df74ca3
andrea-caballeroo <70651799+andrea-caballeroo@users.noreply.github.com>
2020-09-10 10:58:42 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(Celsius)
+    basic.showIcon(IconNames.Happy)
+})
+let Celsius = 0
+basic.showString("Andrea")
+basic.showString("A01285580")
+Celsius = input.temperature()
+basic.forever(function () {
+	
+})
