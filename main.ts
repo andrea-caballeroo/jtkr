c9a4ae15baab14175b109fa34fec1e777881b6ad
andrea-caballeroo <70651799+andrea-caballeroo@users.noreply.github.com>
2020-09-11 12:29:04 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,10 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(Celsius)
     basic.showIcon(IconNames.Happy)
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(Celsius)
+    basic.showIcon(IconNames.Sad)
+})
 let Celsius = 0
 basic.showString("Andrea")
 basic.showString("A01285580")
